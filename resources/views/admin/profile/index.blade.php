d2a87d188e42c68e09ba4fa616f345ae180f6b90
Alucard17th <eddallal.noureddine@gmail.com>
2024-04-30 05:50:55 +0100

    cmt-new

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -197,7 +197,7 @@
                         @endforeach
                     </tbody>
                 </table>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Save</button>
             </form>
         </div>
         <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
