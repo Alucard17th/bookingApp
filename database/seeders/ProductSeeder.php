ba74633e1296a8450b55ad7f0b1c6df9e53a7520
Alucard17th <eddallal.noureddine@gmail.com>
2024-08-02 07:06:42 +0100

    sqd

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
 class ProductSeeder extends Seeder
 {
